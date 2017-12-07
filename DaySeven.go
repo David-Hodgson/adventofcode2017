e37ff0730e681f85eea1ef279bf562c7b6aaf997
David-Hodgson <david.hodgson@feefo.com>
2017-12-07 14:19:34 +0000

    Day Seven - Part Two Complete

--- DaySeven.go
+++ DaySeven.go
@@ -10,7 +10,9 @@ type  Program struct {
 	name     string
 	parent   string
 	children []string
+	childWeights []int
 	weight   int
+	totalWeight int
 }
 
 func parseStringToProgram(input string) Program {
@@ -20,9 +22,9 @@ func parseStringToProgram(input string) Program {
 	name := parts[0]
 	weight,_ := strconv.Atoi(parts[1][1:len(parts[1])-1])
 
-	newprogarm := Program{}
-	newprogarm.name = name
-	newprogarm.weight = weight
+	newProgram := Program{}
+	newProgram.name = name
+	newProgram.weight = weight
 
 	if len(parts) > 2 {
 
@@ -39,9 +41,10 @@ func parseStringToProgram(input string) Program {
 			subProgramList[progNumber] = subprogramName
 			progNumber++
 		}
-		newprogarm.children = subProgramList
+		newProgram.children = subProgramList
 	}
-	return newprogarm
+
+	return newProgram
 }
 
 func findRoot(programStack map[string]Program) Program {
@@ -104,6 +107,56 @@ func DaySevenPartOne(){
 	fmt.Println("Root Program:", findRoot(programStack).name)
 }
 
+func getTotalWeight(program Program, programStack map[string]Program) int {
+
+
+	weight := program.weight
+
+	for i:= 0; i<len(program.children); i++ {
+		weight += getTotalWeight(programStack[program.children[i]], programStack)
+	}
+
+	program.totalWeight = weight
+	programStack[program.name] = program
+
+	return weight
+}
+
+func findUnbalancedPrograms(rootProgram Program, programStack map[string]Program) {
+
+	//if we have two children either could be unbalanced
+	if len(rootProgram.children) == 2 {
+		fmt.Println("Program", rootProgram.name, "has two children")
+	} else {
+
+		childWeights := make(map[int]int)
+
+		for i := 0 ; i < len(rootProgram.children); i++ {
+			childProgram := programStack[rootProgram.children[i]]
+			childWeights[childProgram.totalWeight]++
+		}
+
+		fmt.Println(childWeights)
+
+		if len(childWeights) == 1 {
+			fmt.Println("Program", rootProgram.name, "has incorrect amount")
+		} else {
+			for value, count := range childWeights {
+
+				if count == 1 {
+					for j := 0; j < len(rootProgram.children); j++ {
+						childProgram := programStack[rootProgram.children[j]]
+						if childProgram.totalWeight == value {
+							fmt.Println("Program", childProgram.name, "is different with totalWeight", value, ".Program weight:", childProgram.weight)
+							findUnbalancedPrograms(childProgram, programStack)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 func DaySevenPartTwo(){
 
 	fmt.Println("Day Seven - Part Two")
@@ -118,6 +171,10 @@ func DaySevenPartTwo(){
 		programStack[program.name] = program
 	}
 
-	fmt.Println("Root Program:", findRoot(programStack).name)
-	fmt.Println(programStack)
+	rootName := findRoot(programStack).name
+	rootProgram := programStack[rootName]
+	fmt.Println("Root Program:", rootName)
+
+	fmt.Println("Root wieght:", getTotalWeight(rootProgram, programStack))
+	findUnbalancedPrograms(rootProgram, programStack)
 }
